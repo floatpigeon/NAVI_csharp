93deae42d4051b0dc29281d3a4927c4a8733228c
floatpigeon <floatpigeon@proton.me>
2024-11-26 21:04:43 +0800

    Add class Node and easy test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+using PathPlanner.Node;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Start");
+        Vector2 end = new Vector2(3, 2);
+        Node mynode = new Node(1, 2, null, end);
+
+        mynode.SreachChildren(1.5f, 4, end);
+    }
+}
---
+++ src/PathPlinner/Node.cs
@@ -0,0 +1,72 @@
+using System.Numerics;
+using System.Collections.ObjectModel;
+
+namespace PathPlanner.Node
+{
+    class Node
+    {
+        public Vector2 Position { get; }
+        public Node? Parent { get; }
+        public double ValueH { get; private set; }
+        public double ValueG { get; private set; }
+        public double angle { get; private set; } = 0;
+        public Node(float x, float y, Node? parent, Vector2 end)
+        {
+            Position = new Vector2(x, y);
+            Parent = parent;
+            CalcValueG();
+            CalcValueH(end);
+        }
+        public Node(Vector2 position, Node? parent, Vector2 end)
+        {
+            Position = position;
+            Parent = parent;
+            CalcValueG();
+            CalcValueH(end);
+        }
+        public Collection<Node> SreachChildren(float step, int branch, Vector2 end)
+        {
+            Collection<Node> children = new Collection<Node>();
+            if (this.ValueH <= step)
+            {
+                Node endNode = new Node(end, this, end);
+                children.Add(endNode);
+                Console.WriteLine("Find It");
+                return children;
+            }
+
+            for (int i = 0; i < branch; i++)
+            {
+                double angle = (double)i / branch * 2.0 * Math.PI;
+                Vector2 delta = new Vector2((float)Math.Cos(angle) * step, (float)Math.Sin(angle) * step);
+                Node child = new Node(Position + delta, this, end);
+                child.angle = angle;
+                children.Add(child);
+                Console.WriteLine("{0},{1}", child.Position, child.angle / Math.PI);
+            }
+
+            return children;
+        }
+        private void CalcValueG()
+        {
+            if (Parent == null)
+            {
+                ValueG = 0;
+            }
+            else
+            {
+                double errorX = Position.X - Parent.Position.X;
+                double errorY = Position.Y - Parent.Position.Y;
+                double errorAngle = Math.Abs(angle - Parent.angle);
+                double turnPunish = Math.Log(1 + errorAngle);
+                ValueG = Math.Sqrt(errorX * errorX + errorY * errorY) + 0.2 * turnPunish + Parent.ValueG;
+            }
+        }
+        private void CalcValueH(Vector2 end)
+        {
+            double errorX = Position.X - end.X;
+            double errorY = Position.Y - end.Y;
+            ValueH = Math.Sqrt(errorX * errorX + errorY * errorY);
+        }
+    }
+}
