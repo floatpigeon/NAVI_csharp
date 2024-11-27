f59f3af5560a348789908ff917f2fb410bec4ea3
floatpigeon <floatpigeon@proton.me>
2024-11-28 00:48:13 +0800

    Update works

--- Program.cs
+++ Program.cs
@@ -1,38 +1,38 @@
 ﻿using System.Numerics;
-using PathPlanner;
+using PathPlanner.Nodes;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Start");
-        Vector2 end = new Vector2(0, 0);
-        Node beginNode = new Node(5, 5, null, end);
-        beginNode.SreachChildren(1, 4, end);
+        // Console.WriteLine("Start");
+        // Vector2 end = new Vector2(0, 0);
+        // Node beginNode = new Node(5, 5, null, end);
+        // beginNode.SreachChildren(1, 4, end);
 
         // Vector2 iv = new Vector2(1.4f, 3.3f);
         // HybridAstar a = new HybridAstar();
         // Vector2 ov = a.PositionInWorldToGrid(iv, 1.2f);
         // Console.WriteLine(ov);
 
+        // PriorityQueue<Node, double> OpenList = new PriorityQueue<Node, double>();
 
-        PriorityQueue<Node, double> OpenList = new PriorityQueue<Node, double>();
+        // Node node1 = new Node(1, 0, beginNode, end);
+        // Node node2 = new Node(2, 0, beginNode, end);
+        // Node node3 = new Node(2, 1, beginNode, end);
+        // Node node4 = new Node(2, 2, beginNode, end);
+        // // Console.WriteLine(node1.ValueF);
 
-        Node node1 = new Node(1, 0, beginNode, end);
-        Node node2 = new Node(2, 0, beginNode, end);
-        Node node3 = new Node(2, 1, beginNode, end);
-        Node node4 = new Node(2, 2, beginNode, end);
-        // Console.WriteLine(node1.ValueF);
+        // OpenList.Enqueue(node1, node1.ValueF);
+        // OpenList.Enqueue(node2, node2.ValueF);
+        // OpenList.Enqueue(node3, node3.ValueF);
+        // OpenList.Enqueue(node4, node4.ValueF);
 
-        OpenList.Enqueue(node1, node1.ValueF);
-        OpenList.Enqueue(node2, node2.ValueF);
-        OpenList.Enqueue(node3, node3.ValueF);
-        OpenList.Enqueue(node4, node4.ValueF);
+        // while (OpenList.Count != 0)
+        // {
+        //     Node nodetop = OpenList.Dequeue();
+        //     Console.WriteLine("{0},{1}", nodetop.Position, nodetop.ValueF);
+        // }
 
-        while (OpenList.Count != 0)
-        {
-            Node nodetop = OpenList.Dequeue();
-            Console.WriteLine("{0},{1}", nodetop.Position, nodetop.ValueF);
-        }
     }
 }
---
+++ src/MotionModle/Models/IModel.cs
@@ -0,0 +1,10 @@
+
+using System.Drawing;
+
+namespace MotionModel.Models;
+
+interface IModel
+{
+    public float Speed { get; set; }
+
+}
