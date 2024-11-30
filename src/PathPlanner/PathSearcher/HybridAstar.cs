806dfcf3c889bc6eab1d19f663d4d59540c7e45d
floatpigeon <floatpigeon@proton.me>
2024-11-30 16:40:30 +0800

    Finish HybirdAstar (untest)

--- src/PathPlanner/PathSearcher/HybridAstar.cs
+++ src/PathPlanner/PathSearcher/HybridAstar.cs
@@ -6,50 +6,42 @@ using PathPlanner.Nodes;
 namespace PathPlanner.HybridAstar;
 class HybridAstar
 {
-    public GridMap Map { get; private set; }
-    private PriorityQueue<INode, double> OpenList = new();
     public HybridAstar(GridMap map) { Map = map; }
-
-    public Vector2 PositionInWorldToGrid(Vector2 worldWorldPosition, float size)
-    {
-        int x = (int)(worldWorldPosition.X / size + 0.5);
-        int y = (int)(worldWorldPosition.Y / size + 0.5);
-        return new Vector2(x, y);
-    }
-
     public INode? PathSearch(Vector2 Begin, Vector2 End)
     {
-        Node BeginNode = new Node(Begin, null);
+        OpenList = new();
+        Node BeginNode = new(Begin, null);
+        Node.EndPosition = End;
         OpenList.Enqueue(BeginNode, BeginNode.TotalCost);
 
         while (OpenList.Count != 0)
         {
             INode CurrentNode = OpenList.Dequeue();
-            Vector2 CurrentGrid = PositionInWorldToGrid(CurrentNode.WorldPosition, 1);
+            Vector2 CurrendGrid = Map.PositionInWorldToGrid(CurrentNode.WorldPosition);
 
-            if (Map.GetState(CurrentGrid) == State.CLOSED)
+            if (Map.GetState(Map.PositionInWorldToGrid(CurrentNode.WorldPosition)) == State.CLOSED)
                 continue;
-
             if (CurrentNode.WorldPosition == End)
                 return CurrentNode;
 
-            Map.StateChange(CurrentNode.WorldPosition, State.CLOSED);
-            Collection<INode> Children = CurrentNode.SearchChildren;
+            Map.StateChange(CurrendGrid, State.CLOSED);
+            var Children = CurrentNode.SearchChildren;
 
-            for (int i = 0; i < Children.Count; i++)
+            foreach (var Child in Children)
             {
-                if (Accessible(Children[i]))
-                    OpenList.Enqueue(Children[i], Children[i].TotalCost);
+                if (Accessible(Child))
+                    OpenList.Enqueue(Child, Child.TotalCost);
             }
         }
-        Console.WriteLine("OpenList Empty");
+
+        Console.WriteLine("Path Empty");
         return null;
     }
-
+    private GridMap Map { get; set; }
+    private PriorityQueue<INode, double> OpenList = new();
     private bool Accessible(INode ChildNode)
     {
-        Vector2 ChildGrid = PositionInWorldToGrid(ChildNode.WorldPosition, 1);
-
+        Vector2 ChildGrid = Map.PositionInWorldToGrid(ChildNode.WorldPosition);
         if (!Map.IsValid(ChildGrid))
             return false;
         if (Map.GetState(ChildGrid) == State.CLOSED || Map.GetState(ChildGrid) == State.OBSTACLE)
